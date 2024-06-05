be8971ee1e63d7cf6c6b3af870d700708dcfa3ee
Aaryanani01 <161431693+Aaryanani01@users.noreply.github.com>
2024-06-05 08:12:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", 7171672841))
 MONGO_URL = getenv("MONGO_URL", None)
 SUPPORT_GRP = getenv("SUPPORT_GRP", "HeartStorys1")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "HeartStorys1")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "MoonXtns_Updates")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "its_me_Naav")
 
 # Random Start Images
